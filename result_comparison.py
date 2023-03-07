b3892b65aa30948c6482fe214d8ba1923a33cd10
laihanel <laihanel@hotmail.com>
2023-03-06 23:38:54 -0500

    add comparison method

--- result_comparison.py
+++ result_comparison.py
@@ -75,7 +75,7 @@ def msa_to_embed(msa_path, max_seqs=200, AminoAcids='HETL'):
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_MSA', default='RNA_TESTSET/MSA_pydca/RF00001.faclean', type=str)
+    parser.add_argument('--input_MSA', default='RNA_TESTSET/MSA_pydca/RF01998.faclean', type=str)
     parser.add_argument('--model', default='pretrained_models/model.chk', type=str)
     args = parser.parse_args()
 
@@ -135,7 +135,7 @@ def main():
 
     ### get ground truth for input msa
 
-    test_pdb_data_pickle_file = 'RNA_TESTSET_PDB_DATA.pickle'
+    test_pdb_data_pickle_file = 'RNA_TESTSET_PDB_DATA_.pickle'
     if os.path.exists(test_pdb_data_pickle_file):
         with open(test_pdb_data_pickle_file, 'rb') as handle:
             test_pdb_data = pickle.load(handle)
@@ -146,27 +146,222 @@ def main():
             i, j = k[0], k[1]
             if abs(i-j) > 4:
                 lbl = distance_to_37(v[-1])
-                if lbl <= -1:
-                    lbl2 = 100
-                elif lbl <16:
-                    lbl2 = 1  ##### lbl is 1 (contact) if distance is smaller than 10A, which corresponds to label 0,1,2,...,15
-                elif lbl >= 16:
-                    lbl2 = 0 ##### lbl is 0 (non-contact) if distance is larger than 10A, which corresponds to label 16,17,18,....
-                test_label[i, j] = lbl2
-                test_label[j, i] = lbl2
+                test_label[i, j] = lbl
+                test_label[j, i] = lbl
+        test_label = torch.from_numpy(test_label).long().unsqueeze(0)
 
+        test_label = test_label.cpu().squeeze(0) - mask
+        test_label[test_label <= -1] = 100
+        test_label[test_label < 16] = 1  ##### lbl is 1 (contact) if distance is smaller than 10A, which corresponds to label 0,1,2,...,15
+        test_label[test_label >= 16] = 0
+        ppv = (pred * test_label).sum() / int(2 * 1 * L)  ##### position-wise multiplication to find "positive prediction", divided by 2L (total number of predictions)
+        print(rna_fam_name, ppv.item())
         test_label[test_label == -100] = 0
 
         # test_label = torch.from_numpy(test_label).long().unsqueeze(0)
         print(f"labels pre-processed: {L} x {L} matrix with 37/2 classes!")
         pred = pred.numpy().astype(int)
+        test_label = test_label.numpy().astype(int)
 
-        element_compare = pred== test_label
 
-        plt.imshow(element_compare * 1)
-        plt.colorbar()
+        # #
+        # plt.subplot(1, 2, 1)
+        # plt.imshow(pred)
+        # plt.colorbar()
+        # plt.title("Prediction = 120")
+        # #
+        # plt.subplot(1, 2, 2)
+        # plt.imshow(test_label)
+        # plt.colorbar()
+        # plt.title("GT")
+        # plt.show()
+        #
+        # plt.imshow(element_compare * 1)
+        # plt.colorbar()
+        # plt.title("Prediction va GT")
+        # plt.show()
+        #
+        # plt.imshow(element_compare * 1)
+        # plt.colorbar()
+        # plt.title("Prediction va GT")
+        # plt.show()
+
+        from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+        cm = confusion_matrix(test_label.flatten(), pred.flatten())/2
+        cmd_obj = ConfusionMatrixDisplay(cm, display_labels=['0', '1'])
+        cmd_obj.plot()
+        cmd_obj.ax_.set(
+            title=f'Confusion Matrix for {rna_fam_name}, Total Length: {L}',
+            xlabel='Predicted Contact',
+            ylabel='Actual Contact')
         plt.show()
 
+        result = np.zeros((L, L))
+
+        i, j = np.where((pred == 1) & (pred != test_label))
+        pred_wrong = list(zip(i.tolist(),j.tolist()))
+        pred_wrong_list = []
+        for (x, y) in pred_wrong:
+            if (y, x) in pred_wrong:
+                pred_wrong_list.append((x, y))
+                pred_wrong.remove((y, x))
+
+        k, l = np.where((pred == 1) & (pred == test_label))
+        pred_correct = list(zip(k.tolist(), l.tolist()))
+        pred_correct_list = []
+        for (x, y) in pred_correct:
+            if (y, x) in pred_correct:
+                pred_correct_list.append((x, y))
+                pred_correct.remove((y, x))
+
+        n, m = np.where(pred == 1)
+        prediction = list(zip(n.tolist(), m.tolist()))
+        pred_list = []
+        for (x, y) in prediction:
+            if (y, x) in prediction:
+                pred_list.append((x, y))
+                prediction.remove((y, x))
+
+        for index in pred_list:
+            result[index] = 1
+
+        # plt.imshow(result * 1)
+        # plt.colorbar()
+        # plt.title("Prediction Result")
+        # plt.show()
+
+        number_of_cuts1 = []
+        cut_result = {}
+        for contact in pred_list:
+            c_i, c_j = contact
+            number_of_cut = 0
+            for predicted in pred_list:
+                p_i, p_j = predicted
+                if (c_i <= p_i and c_j < p_j and c_j > p_i) or (p_i < c_i and p_j <= c_j and p_j > c_i) :
+                    result[c_i, c_j] = 1
+                    number_of_cut += 1
+                else:
+                    continue
+            cut_result[contact] = number_of_cut
+            number_of_cuts1.append(number_of_cut)
+
+        # plt.plot(np.linspace(0, L, num=L), number_of_cuts1, color='maroon')
+        # plt.ylim((0, 30))
+        # plt.title(f"Number of Cuts for {rna_fam_name}:{sum(number_of_cuts1)}")
+        # plt.show()
+
+        number_of_cuts2 = []
+        cut_result = {}
+        for contact in pred_wrong_list:
+            c_i, c_j = contact
+            number_of_cut = 0
+            for predicted in pred_list:
+                p_i, p_j = predicted
+                if (c_i <= p_i and c_j < p_j and c_j > p_i) or (p_i < c_i and p_j <= c_j and p_j > c_i) :
+                    result[c_i, c_j] = 1
+                    number_of_cut += 1
+                else:
+                    continue
+            cut_result[contact] = number_of_cut
+            number_of_cuts2.append(number_of_cut)
+
+        # plt.plot(np.linspace(0, len(pred_wrong_list), num=len(pred_wrong_list)), number_of_cuts2, color='maroon')
+        # plt.ylim((0, 30))
+        # plt.title(f"Number of Cuts for {rna_fam_name} for Wrong Prediction: {sum(number_of_cuts2)}")
+        # plt.show()
+
+
+        number_of_cuts3 = []
+        cut_result = {}
+        for contact in pred_correct_list:
+            c_i, c_j = contact
+            number_of_cut = 0
+            for predicted in pred_list:
+                p_i, p_j = predicted
+                if (c_i <= p_i and c_j < p_j and c_j > p_i) or (p_i < c_i and p_j <= c_j and p_j > c_i) :
+                    result[c_i, c_j] = 1
+                    number_of_cut += 1
+                else:
+                    continue
+            cut_result[contact] = number_of_cut
+            number_of_cuts3.append(number_of_cut)
+
+
+        # plt.plot(np.linspace(0, len(pred_correct_list), num=len(pred_correct_list)), number_of_cuts3, color='maroon')
+        # plt.ylim((0, 30))
+        # plt.title(f"Number of Cuts for {rna_fam_name} for Correct Prediction: {sum(number_of_cuts3)}")
+        # plt.show()
+
+        plt.figure(figsize=(10, 6))
+        plt.boxplot([number_of_cuts1, number_of_cuts2, number_of_cuts3], vert=False, showmeans=True)
+        plt.title(f"Total number of Cuts for {rna_fam_name}: {sum(number_of_cuts1)}, Wrong Prediction: {sum(number_of_cuts2)}, Correct Prediction: {sum(number_of_cuts3)}")
+        plt.yticks([1, 2, 3], ['All', 'Wrong', 'Correct'])
+        plt.show()
+
+        plt.style.use('seaborn-deep')
+        plt.figure(figsize=(10, 6))
+        number_of_bins = int(max(number_of_cuts1 + number_of_cuts2 + number_of_cuts3)/2)
+        kwargs = dict(alpha=0.5, bins=number_of_bins)
+        hist_muti = [number_of_cuts1, number_of_cuts2, number_of_cuts3]
+        plt.hist(hist_muti, bins=number_of_bins, histtype='bar', label=['all', 'wrong', 'collect'])
+        # plt.hist(number_of_cuts1, **kwargs, color='r', label='all', histtype='bar')
+        # plt.hist(number_of_cuts3, **kwargs, color='g', label='correct', histtype='bar')
+        # plt.hist(number_of_cuts2, **kwargs, color='b', label='wrong', histtype='bar')
+        plt.gca().set(title=f'Frequency Histogram of Number of Cuts \n Total number of Cuts for {rna_fam_name}: {sum(number_of_cuts1)}, Wrong Prediction: {sum(number_of_cuts2)}, Correct Prediction: {sum(number_of_cuts3)}', ylabel='Frequency', xlabel='Number of cuts')
+        plt.xlim(0, number_of_bins*2+1)
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+
+
+        #
+        # number_of_cuts = []
+        # cut_result = {}
+        # for contact in pred_list:
+        #     c_i, c_j = contact
+        #     number_of_cut = 0
+        #     for predicted in pred_wrong_list:
+        #         p_i, p_j = predicted
+        #         if (c_i < p_i and c_j < p_j and p_j > c_i) or (p_i < c_i and p_j < c_j and p_j > c_i) :
+        #             result[c_i, c_j] = 1
+        #             number_of_cut += 1
+        #         else:
+        #             continue
+        #     cut_result[contact] = number_of_cut
+        #     number_of_cuts.append(number_of_cut)
+        #
+        # plt.plot(np.linspace(0, L, num=L), number_of_cuts, color='maroon')
+        # plt.ylim((0, 30))
+        # plt.title(f"Number of Cuts for {rna_fam_name} by Wrong Prediction: {sum(number_of_cuts)}")
+        # plt.show()
+        #
+        #
+        #
+        # number_of_cuts = []
+        # cut_result = {}
+        # for contact in pred_list:
+        #     c_i, c_j = contact
+        #     number_of_cut = 0
+        #     for predicted in pred_correct_list:
+        #         p_i, p_j = predicted
+        #         if (c_i < p_i and c_j < p_j and p_j > c_i) or (p_i < c_i and p_j < c_j and p_j > c_i) :
+        #             result[c_i, c_j] = 1
+        #             number_of_cut += 1
+        #         else:
+        #             continue
+        #     cut_result[contact] = number_of_cut
+        #     number_of_cuts.append(number_of_cut)
+        #
+        # plt.plot(np.linspace(0, L, num=L), number_of_cuts, color='maroon')
+        # plt.ylim((0, 30))
+        # plt.title(f"Number of Cuts for {rna_fam_name} by Correct Prediction: {sum(number_of_cuts)}")
+        # plt.show()
+
+        # plt.imshow(result * 1)
+        # plt.colorbar()
+        # plt.title(f"blue: wrong prediction - {result[result==1].sum()}, yellow: right GT crossed GT - {result[result==2].sum()}")
+        # plt.show()
 
 
 
